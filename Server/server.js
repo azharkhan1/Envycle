e1e798bdb6f7b2d2fbc35413a4a8da3ca6a42c69
azharkhan1 <mazhar@digital-dividend.com>
2021-12-17 14:24:38 +0500

    test

--- Server/server.js
+++ Server/server.js
@@ -34,6 +34,7 @@ app.use(bodyParser.json());
 app.use(cors({
     origin: ["http://localhost:3000", 'https://envycle.herokuapp.com'],
     credentials: true,
+    exposedHeaders: ["set-cookie"]
 }));
 
 
